adeef73cac2c613252a6914dd9da39430503dfb4
hyperkai <kazya.ito.dream@gmail.com>
2021-02-28 04:02:22 +0900

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,4 +124,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'example@gmail.com' # Put your email address
-EMAIL_HOST_PASSWORD = 'example' # Put your email password
+EMAIL_HOST_PASSWORD = 'abcdefg' # Put your email password
